4fedfc2c619c99a984e217de928f165a1715244e
beckhamv_v <beckhamv_v@922dabf9-ac34-0510-860a-9fefbfe4c3fc>
2016-06-09 06:28:46 +0000

    메인 및 회원가입 -준수

--- Project/src/spring/control/RegControl.java
+++ Project/src/spring/control/RegControl.java
@@ -1,20 +1,43 @@
 package spring.control;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import mybatis.dao.TestDAO;
 import mybatis.vo.TestVO;
 
-
 @Controller
 public class RegControl {
 
+	@Autowired
+	private TestDAO tdao;
 	
-	@RequestMapping("/reg.inc")
+	@Autowired
+	HttpServletRequest request;
+	
+	
+	// 메인
+	@RequestMapping("/main.inc")
 	public String main(){
-		return "/join";
+		return "/main";
 	}
+	
+	
+	// 회원가입
+	@RequestMapping(value="/reg_ok.inc",method=RequestMethod.POST)
+	public ModelAndView add(TestVO vo){
+	
+		
+		TestVO vo1 = tdao.addMember(vo);	
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/main");
+		return mv;
+	}
+	
 }
