66c38a93bdad876cbb23836806f84ade54ef1e2a
ows88 <ows88@922dabf9-ac34-0510-860a-9fefbfe4c3fc>
2016-06-10 00:09:00 +0000

    --지난경기 불러오기 기능 추가--

--- Project/src/spring/control/GameBuyControl.java
+++ Project/src/spring/control/GameBuyControl.java
@@ -2,6 +2,7 @@
 	제 목 : GameBuyControl.java
 	역 할 : 게임구매 탭의 기능을 수행할 컨트롤러
 	로 그 : 1.프로그램 최초 생성,당일 경기 리스트 불러오기 구현 (오우석 2016/06/09)
+			2.지난경기 불러오기 기능 추가(오우석 2016/06/10)
 	*/
 package spring.control;
 
@@ -27,6 +28,17 @@ public class GameBuyControl {
 	
 	@RequestMapping("/todaylist.inc")
 	public ModelAndView showList(){
+		ModelAndView mv = new ModelAndView();
+		MatchVO[] games = mDao.scheduleToday();
+		
+		mv.addObject("games", games);
+		mv.setViewName("oppGameList");
+		return mv;
+	}
+	
+	//지난경기 불러오기(오우석 2016/06/10)
+	@RequestMapping("/endlist.inc")
+	public ModelAndView endShowList(){
 		ModelAndView mv = new ModelAndView();
 		MatchVO[] games = mDao.scheduleToday();
 		
