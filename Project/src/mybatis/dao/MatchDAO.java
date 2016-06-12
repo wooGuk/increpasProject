f1f0177a06e70f2d237e23c0a92d3dc5c0e36296
ows88 <ows88@922dabf9-ac34-0510-860a-9fefbfe4c3fc>
2016-06-12 03:52:11 +0000

    메소드 추가및 수정 -getToatalCount 추가 -scheduleEnd() 수정

--- Project/src/mybatis/dao/MatchDAO.java
+++ Project/src/mybatis/dao/MatchDAO.java
@@ -17,6 +17,9 @@ import mybatis.vo.TestVO;
 	역 할 : Match DB에서 경기 일정을 가져오는 역할을 한다.
 	로 그 : 1.프로그램 최초 생성 (오우석 2016/06/09)
 			2.당일 경기, 지난 경기일자 불러오기(오우석 2016/06/09)
+			3.getToatalCount() 메소드 추가, scheduleEnd() 메소드 수정(오우석 2016/06/12)
+			  1)getToatalCount() 은 지난 경기 데이터의 수가 총 몇인지 확인하려는 데이터
+			  2)scheduleEnd() 정해진 갯수 만큼 가져 와야 되기 때문 map구조를 이용
 */
 public class MatchDAO {
 
@@ -25,6 +28,14 @@ public class MatchDAO {
 	
 
 	/* 비니지스 로직들 */
+	
+	// 전체게신물의 수를 반환하는 메소드 오우석 2016/06/12
+	public int getToatalCount() {
+		// 인자값은 게시판의 종류이다. 게시판의 값은 "BBS"라고 정한다.
+		return template.selectOne("match.totalCount");
+	}
+	
+	
 	//오늘일자 경기 불러오기 오우석 2016/06/09
 	public MatchVO[] scheduleToday(){
 		
@@ -39,12 +50,11 @@ public class MatchDAO {
 		return games;
 	}
 	//지난경기일자 불러오기 오우석 2016/06/09
-	public MatchVO[] scheduleEnd(){
-		
-		List<MatchVO> list = new ArrayList<MatchVO>();
+	public MatchVO[] scheduleEnd(Map<String, String> map){
+		//페이징을 위한 begin과 end가 있어야 함으로 map구조를 이용하는 것으로 변경 오우석 2016/06/12
+		List<MatchVO> list = template.selectList("match.matchEnd",map);
 		MatchVO[] games = null;
 		
-		list = template.selectList("match.matchEnd");
 		if(list != null || !(list.isEmpty())){
 			games = new MatchVO[list.size()];
 			list.toArray(games);
