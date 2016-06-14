7bb79b7e090eec836f7b85b4065a3ece29673e0b
beckhamv_v <beckhamv_v@922dabf9-ac34-0510-860a-9fefbfe4c3fc>
2016-06-14 08:55:38 +0000

    getter가 빠져서 getter 추가 -준수

--- Project/src/mybatis/vo/MemberVO.java
+++ Project/src/mybatis/vo/MemberVO.java
@@ -1,53 +1,55 @@
 package mybatis.vo;
 
+
+/*
+제 목 : MemberVO
+역 할 : 
+로 그 : 완성판 getter가 빠짐.
+		ㅡ> getter 추가
+*/
+
+
 public class MemberVO {
 
 	private String id, password, phone,name, address;
 	private int coin;
-
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getCoin() {
+		return coin;
+	}
 	public void setCoin(int coin) {
 		this.coin = coin;
 	}
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
 	
 	
 }
