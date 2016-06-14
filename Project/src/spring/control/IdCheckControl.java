c7aa78e69f5cbd273e49a740abc2244789e3e0ac
beckhamv_v <beckhamv_v@922dabf9-ac34-0510-860a-9fefbfe4c3fc>
2016-06-14 08:33:33 +0000

    ID체크 컨트롤러 -준수

--- Project/src/spring/control/IdCheckControl.java
+++ Project/src/spring/control/IdCheckControl.java
@@ -7,31 +7,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import mybatis.dao.TestDAO;
-import mybatis.vo.TestVO;
+import mybatis.dao.MemberDAO;
+import mybatis.vo.MemberVO;
 
 	/*
 	제 목 : IdCheckControl
 	역 할 : 아이디 중복확인을 하는 역할입니다.
-	로 그 : 프로그램 최초 생성 (박상원 2016/06/13)
+	로 그 : 프로그램 최초 생성 (장준수 2016/06/13)
 	*/
 @Controller
 public class IdCheckControl {
 	
 	@Autowired
-	TestDAO tdao;
+	MemberDAO tdao;
 
 	@Autowired
 	HttpServletRequest request;
 	
 	@RequestMapping("/idCheck.inc")
 	public ModelAndView idcheck(String id){
+		System.out.println(id);
+		MemberVO vo = tdao.idCheck(id);
 		
-		TestVO vo = tdao.idCheck(id);
-	
 		ModelAndView mv = new ModelAndView();
 		if(vo == null)
-			mv.addObject("chk", "0"); //사용할 수 있는 ID
+			mv.addObject("chk", "0");
 		else
 			mv.addObject("chk", "1");//사용할 수 없는 ID
 		
