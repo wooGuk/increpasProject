b6b4c8555f9bb04439c502d13734a02c34cb5516
assa_sh <assa_sh@922dabf9-ac34-0510-860a-9fefbfe4c3fc>
2016-06-20 10:14:26 +0000

    teamDAO

---
+++ Project/src/mybatis/dao/TeamDAO.java
@@ -0,0 +1,36 @@
+	/*
+	제 목 : TeamDAO
+	역 할 : team db 테이블 접근 클래스
+	로 그 : 프로그램 최초 생성 (정성훈 2016/06/20)
+	*/
+
+package mybatis.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import mybatis.vo.MatchVO;
+import mybatis.vo.TeamVO;
+
+public class TeamDAO {
+	@Autowired
+	SqlSessionTemplate template;
+	
+	//비지니스 로직들
+	
+	//팀정보 가져오기
+	public TeamVO[] getTeamInfo(){
+		List<TeamVO> list = new ArrayList<TeamVO>();
+		TeamVO[] teams = null;
+		
+		list = template.selectList("team.all");
+		if(list != null || !(list.isEmpty())){
+			teams = new TeamVO[list.size()];
+			list.toArray(teams);
+		}
+		return teams;
+	}
+}
