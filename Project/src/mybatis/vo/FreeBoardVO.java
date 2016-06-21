4fbe31a7f7c1db2a26c09229f96b600776e85b04
beckhamv_v <beckhamv_v@922dabf9-ac34-0510-860a-9fefbfe4c3fc>
2016-06-21 08:05:29 +0000

    자유게시판VO 추가 -준수

---
+++ Project/src/mybatis/vo/FreeBoardVO.java
@@ -0,0 +1,130 @@
+package mybatis.vo;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FreeBoardVO {
+	
+	private String seq,title,content,hit,groups,step,lev,bname,regdate,
+	uploadFileName,ip,status;
+	
+	private int nowPage;
+	
+	// 스프링환경에서 첨부된 파일을 받을 때는 반드시 다음의 자료형으로 받아야한다.
+	private MultipartFile upload;
+
+	public String getSeq() {
+		return seq;
+	}
+
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+
+	
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	
+	public String getHit() {
+		return hit;
+	}
+
+	public void setHit(String hit) {
+		this.hit = hit;
+	}
+
+	public String getGroups() {
+		return groups;
+	}
+
+	public void setGroups(String groups) {
+		this.groups = groups;
+	}
+
+	public String getStep() {
+		return step;
+	}
+
+	public void setStep(String step) {
+		this.step = step;
+	}
+
+	public String getLev() {
+		return lev;
+	}
+
+	public void setLev(String lev) {
+		this.lev = lev;
+	}
+
+	public String getBname() {
+		return bname;
+	}
+
+	public void setBname(String bname) {
+		this.bname = bname;
+	}
+
+	public String getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+
+	public String getUploadFileName() {
+		return uploadFileName;
+	}
+
+	public void setUploadFileName(String uploadFileName) {
+		this.uploadFileName = uploadFileName;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public int getNowPage() {
+		return nowPage;
+	}
+
+	public void setNowPage(int nowPage) {
+		this.nowPage = nowPage;
+	}
+
+	public MultipartFile getUpload() {
+		return upload;
+	}
+
+	public void setUpload(MultipartFile upload) {
+		this.upload = upload;
+	}
+	
+}
