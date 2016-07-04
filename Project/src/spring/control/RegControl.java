bf8a493f89b115b37dbb2cd0218c212e425f27b7
beckhamv_v <beckhamv_v@922dabf9-ac34-0510-860a-9fefbfe4c3fc>
2016-07-04 09:03:23 +0000

    컨트롤러 정리 -준수

--- Project/src/spring/control/RegControl.java
+++ Project/src/spring/control/RegControl.java
@@ -19,6 +19,7 @@ import mybatis.vo.MemberVO;
 	로 그 :  1.프로그램 최초 생성 (장준수 2016/06/14)
 				2.main.inc 매핑 MainControl로 이동시킴 (정성훈 2016/06/16)
 					3. setViewName을 redirect로 수정 (2016/06/16 장준수)
+					4. id체크 추가 (2016/07/04 장준수)
 	 */		
 
 
@@ -43,6 +44,23 @@ public class RegControl {
 		mv.addObject("vo", vo1);
 		/*mv.setViewName("/main");*/
 		mv.setViewName("redirect:/main.inc");
+		return mv;
+	}
+	
+	// 아이디 중복확인
+	@RequestMapping("/idCheck.inc")
+	public ModelAndView idcheck(String id){
+		System.out.println(id);
+		MemberVO vo = tdao.idCheck(id);
+		
+		ModelAndView mv = new ModelAndView();
+		if(vo == null)
+			mv.addObject("chk", "0");
+		else
+			mv.addObject("chk", "1");//사용할 수 없는 ID
+		
+		mv.setViewName("/idCheck");
+		
 		return mv;
 	}
 	
